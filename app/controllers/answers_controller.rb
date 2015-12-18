2334527da1d6f3da8f080c96dfa460968b058c14
Nathan Riemer <nathan.riemer@gmail.com>
2015-12-18 16:47:36 -0500

    Adds entry form for questionnaire answers

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def create 
+    application = Application.find(params[:application_id])
+
+    answers = params[:answers]
+    answers.each_pair do |question_id, answer_content|
+      Answer.create(question_id: question_id, content: answer_content, user_id: session[:user_id], application: application)
+    end
+
+    redirect_to application
+  end
+end
