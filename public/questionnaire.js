f3a7311282c51e89d0893c875dccb5715645e697
Nathan Riemer <nathan.riemer@gmail.com>
2015-12-17 19:58:41 -0500

    Adds ability to delete questions on questionnaire edit page

--- public/questionnaire.js
+++ public/questionnaire.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
       success: function(data){
         if (data.created) {
           $('input.new').val('');
-          var newDiv = "<div><p>"+ qContent + "</p><button class='edit question'>Edit Question</button><button class='delete question'>Remove</button></div>"
+          var newDiv = "<div data-question-id=" + data.questionId + "><p>"+ qContent + "</p><button class='edit question'>Edit Question</button><button class='delete question'>Remove</button></div>"
           $(newDiv).appendTo('.questions');
           console.log("Question successfully added");
         } else {
@@ -45,7 +45,7 @@ $(document).ready(function() {
     $updateButton.click(function(event) {
       submitUpdate(event);
     });
-  }
+  };
 
   var submitUpdate = function(event) {
     console.log("clicked update button");
@@ -79,9 +79,29 @@ $(document).ready(function() {
         console.log(error);
       }
     });
-
   };
 
-
-
+  $('button.delete.question').click(function(event) {
+    console.log('clicked delete button');
+    var $parentDiv = $(event.target).parent();
+    var questionID = $parentDiv.data("question-id");
+    var questionnaireID = $(event.target).parent().parent().data("questionaire-id");
+    console.log(questionID);
+    jQuery.ajax({
+      url: "/questionnaires/" + questionnaireID +  "/questions/" + questionID,
+      method: "DELETE",
+      dataType: "json",
+      success: function(data){
+        if (data.deleted) {
+          $parentDiv.remove();
+          console.log("Question successfully deleted");
+        } else {
+          console.log("Not deleted");
+        }
+      }, 
+      error: function(error) {
+        console.log(error);
+      }
+    });    
+  });
 });
