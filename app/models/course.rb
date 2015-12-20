1a2d25ff62a2fc0199f59d396d3d215dfecd9e8b
Nathan Riemer <nathan.riemer@gmail.com>
2015-12-20 16:00:11 -0500

    Adds autoquestionnaire creation to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :officers
   has_many :questionnaires
   has_many :applications, :dependent => :delete_all
+  after_create :create_questionnaires
 
   def instructor_questionnaire
     questionnaires.find { |questionnaire| questionnaire.questionnaire_type == 'Instructor'}
@@ -12,4 +13,9 @@ class Course < ActiveRecord::Base
     questionnaires.find { |questionnaire| questionnaire.questionnaire_type == 'Officer'}
   end
 
+  protected
+    def create_questionnaires
+      questionnaires.create(questionnaire_type: "Instructor")
+      questionnaires.create(questionnaire_type: "Officer")
+    end
 end
